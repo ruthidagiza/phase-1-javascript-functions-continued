aef391bb03cfd3f15887b14b5c6495b8347f5ddf
IDAGIZA <ruthidagiza@gmail.com>
2022-06-11 14:38:37 +0300

    Done.

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ function mondayWork(activity = 'go to the office'){
  return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective (d){
- let result = function (a){
+/*function wrapAdjective (d = "*"){
+ let result = function (a= 'a hard worker'){
     return `You are ${d}${a}${d}!`
  }
  return result
 }
+ */
 
+let wrapAdjective = function (name="*"){
+  return  function (wrap="a hard worker"){
+      return `You are ${name} ${wrap} ${name}! `
+  }
+}
+console.log(wrapAdjective()());
